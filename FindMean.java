e71083df79165686bfdf9b1539d4bf88be982251
Noda Sergey <nzerg@ya.ru>
2016-10-23 19:49:52 +0500

    Create FindMean.java

---
+++ FindMean.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by Zerg on 23.10.2016.
+ */
+public class FindMean {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите целое число а");
+        int a = scan.nextInt();
+        System.out.println("Введите целое число b");
+        int b = scan.nextInt();
+        System.out.println("Введите целое число c");
+        int c = scan.nextInt();
+        int d = (b + a + c)/3;
+        System.out.println("Среднее значение этих трех чисел равно " + d);
+
+    }
+}
