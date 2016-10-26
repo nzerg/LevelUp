5841827f21a52b0b6eecab83888d8c1eb02d4a45
Noda Sergey <nzerg@ya.ru>
2016-10-26 13:06:32 +0500

    Update Liniya.java

--- Liniya.java
+++ Liniya.java
@@ -4,9 +4,18 @@ public class Liniya {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите целое число а");
-        int a = scanner.nextInt();
+        if(scanner.hasNextInt()) { 
+        int a = scanner.nextInt();}
+        else {
+          System.out.println("Вы ввели не целое число");
+        }
         System.out.println("Введите целое число b");
-        int b = scanner.nextInt();
+        if(scanner.hasNextInt()) { 
+        int b = scanner.nextInt(); }
+        else {
+          System.out.println("Вы ввели не целое число");
+        }
+      
        double x = -b/a;
         System.out.println(x);
 
