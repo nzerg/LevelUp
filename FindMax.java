0e99e2a5ca3c975bd9cd90e957cc3bf3ece3c632
nzerg <nzerg@ya.ru>
2016-11-01 15:34:05 +0500

    1

---
+++ FindMax.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Zerg on 01.11.2016.
+ */
+public class FindMax {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 5, 9, 100, 1000};
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+        }
+        System.out.println("Максимальное число в массиве равно " + max);
+    }
+}
